7ca93248f5ca965086a26134cc57c003c8fb2a3f
Kayla <keoliva@andrew.cmu.edu>
2019-01-25 16:14:18 -0500

    chore: included relevant client files and folders from boilermaker. now the page looks like boilermaker

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,15 @@
-import React, {Component} from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
-class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: ''
-  }
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({response: res.express}))
-      .catch(err => console.log(err))
-  }
-  callApi = async () => {
-    const response = await fetch('/api/hello')
-    const body = await response.json()
-    if (response.status !== 200) throw Error(body.message)
-    return body
-  }
-  handleSubmit = async e => {
-    e.preventDefault()
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({post: this.state.post})
-    })
-    const body = await response.text()
-    this.setState({responseToPost: body})
-  }
+import {Navbar} from './components'
+import Routes from './routes'
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({post: e.target.value})}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes />
+    </div>
+  )
 }
 
 export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import {Provider} from 'react-redux'
+import {Router} from 'react-router-dom'
+import history from './history'
+import store from './store'
 import App from './App'
+import './index.css'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// establishes socket connection
+import './socket'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
