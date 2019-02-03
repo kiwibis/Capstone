14fcd61de40e0489bc59f0bbf3f669f88b3b5d3f
Kayla <keoliva@andrew.cmu.edu>
2019-02-03 05:25:21 -0500

    feat: edit distance with trie

--- server/util/editDistance/trie.js
+++ server/util/editDistance/trie.js
@@ -1,9 +1,9 @@
 class Trie {
   constructor(character = '') {
-    this.data = character
+    this.letter = character
     this.children = {}
     this.size = 0
-    this.isTerminal = false
+    this.word = null
   }
 
   insert(word) {
@@ -15,10 +15,65 @@ class Trie {
 
       node = newNode
     }
-    node.isTerminal = true
+    node.word = word
   }
 
-  search(prefix) {}
+  search(word, maxDistance) {
+    const initialRow = [...Array(word.length + 1).keys()]
+    const candidates = []
+    for (const letter of Object.keys(this.children)) {
+      this.searchHelper(
+        this.children[letter],
+        word,
+        initialRow,
+        maxDistance,
+        candidates
+      )
+    }
+    // candidates are of the form [word, distance]
+    // we want the results in descending order in terms of distance
+    candidates.sort((a, b) => a[1] - b[1])
+    return candidates
+  }
+
+  searchHelper(node, word, previousRow, maxDistance, candidates) {
+    /** build a row for the current letter (node.letter) in the trie, where
+     * we're comparing it against the letters in the word. In the first index,
+     * we're comparing the letter to an empty string
+     */
+    const currentRow = [previousRow[0] + 1],
+      columns = previousRow.length
+    for (let column = 1; column < columns; ++column) {
+      const insertCost = currentRow[column - 1] + 1
+      const deleteCost = previousRow[column] + 1
+      const swapCost =
+        node.letter === word[column - 1]
+          ? previousRow[column - 1]
+          : previousRow[column - 1] + 1
+
+      currentRow.push(Math.min(...[insertCost, deleteCost, swapCost]))
+    }
+
+    // if the last entry in the row indicates the optimal cost is less than the
+    // maximum cost, and there is a word in this trie node, then add it.
+    if (currentRow[columns - 1] <= maxDistance && node.word) {
+      candidates.push([node.word, currentRow[columns - 1]])
+    }
+
+    // if any entries in the row are less than the maximum cost, then
+    // recursively search each brMath.anch of the trie
+    if (Math.min(...currentRow) <= maxDistance) {
+      for (const letter of Object.keys(node.children)) {
+        this.searchHelper(
+          node.children[letter],
+          word,
+          currentRow,
+          maxDistance,
+          candidates
+        )
+      }
+    }
+  }
 }
 
 module.exports = Trie
--- server/util/editDistance/trie.spec.js
+++ server/util/editDistance/trie.spec.js
@@ -9,8 +9,11 @@ describe('Trie', function() {
   })
 
   describe('the head of a trie is initialized with the correct values', function() {
-    it('has the correct data', function() {
-      expect(trie.data).to.equal('')
+    it('has the correct data/letter', function() {
+      expect(trie.letter).to.equal('')
+    })
+    it("there's no word", function() {
+      expect(trie.word).to.equal(null)
     })
     it('has the correct size', function() {
       expect(trie.size).to.equal(0)
@@ -27,8 +30,8 @@ describe('Trie', function() {
     it('updates the size at the head', function() {
       expect(trie.size).to.equal(1)
     })
-    it('the last character inserted is made a terminal node', function() {
-      expect(trie.children.h.children.i.isTerminal).to.equal(true)
+    it('the last character inserted is made a terminal node and stores the word', function() {
+      expect(trie.children.h.children.i.word).to.equal('hi')
     })
     describe('inserts another string with an existing prefix in the trie, correctly', function() {
       beforeEach(function() {
@@ -41,19 +44,38 @@ describe('Trie', function() {
 
         expect(trie.children.h.children.i.children.n.size).to.equal(2)
       })
-      it('the last character is made a terminal node', function() {
+      it('the last character inserted is made a terminal node and stores the word', function() {
+        expect(trie.children.h.children.i.children.n.children.t.word).to.equal(
+          'hint'
+        )
         expect(
-          trie.children.h.children.i.children.n.children.t.isTerminal
-        ).to.equal(true)
+          trie.children.h.children.i.children.n.children.d.children.e.children.r
+            .word
+        ).to.equal('hinder')
       })
     })
   })
 
-  describe('searches for words with a certain prefix', function() {
+  describe('search returns words within input distance of the input word', function() {
     beforeEach(function() {
-      trie.insert('hi')
-      trie.insert('hint')
-      trie.insert('hinder')
+      trie.insert('goober')
+      trie.insert('goobers')
+      trie.insert('gooier')
+      trie.insert('intention')
+      trie.insert('trials')
+    })
+    it('returns the results in ascending order of the distance from the input word', function() {
+      expect(trie.search('goober', 1)).to.deep.equal([
+        ['goober', 0],
+        ['goobers', 1],
+        ['gooier', 1]
+      ])
+      expect(trie.search('execution', 5)).to.deep.equal([['intention', 5]])
+      expect(trie.search('zeil', 4)).to.deep.equal([['trials', 4]])
+      expect(trie.search('zeil', 2)).to.deep.equal([])
+      trie.insert('man')
+      trie.insert('mad')
+      expect(trie.search('moon', 3)).to.deep.equal([['man', 2], ['mad', 3]])
     })
   })
 })
