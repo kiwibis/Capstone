54efc2897b6a36b5d2d858e5f345dc32f5afb84f
Kayla <keoliva@andrew.cmu.edu>
2019-02-06 13:08:45 -0500

    made edit code an HOC. edit-page is working as before

--- client/components/edit-page/edit-page.js
+++ client/components/edit-page/edit-page.js
@@ -1,22 +1,19 @@
 import React, {Component} from 'react'
 import PhotoCapture from '../photo-capture'
 import {connect} from 'react-redux'
-import {submitEditedText} from '../../store'
 import history from '../../history'
 import InputOutputWrapper from './input-output-wrapper'
 import CodeMirror from './code-mirror'
-import jBeautify from 'js-beautify'
 import findOrientation from 'exif-orientation'
-import Evaluator from '../../util/evaluator'
 import Grid from '@material-ui/core/Grid'
 import {withStyles} from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 import Paper from '@material-ui/core/Paper'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import NProgress from 'nprogress'
 import ErrorPage from './error-page'
 import Loading from './loading-page'
 import {Typography} from '@material-ui/core'
+import editCode from '../edit-code'
 
 const styles = theme => ({
   bigGrid: {
@@ -83,68 +80,23 @@ class EditPage extends Component {
   constructor() {
     super()
     this.state = {
-      editedText: '',
-      testCases: undefined,
-      outputs: [],
       image: null,
-      imageClass: '',
-      testCasesRunning: false
+      imageClass: ''
     }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
     this.readFile = this.readFile.bind(this)
-    this.evaluator = new Evaluator()
   }
 
   componentDidMount() {
     if (!this.props.loading && !this.props.image) return history.push('/')
-    NProgress.configure({parent: '#inputOutput'})
     this.readFile()
-  }
-
-  handleChange(inputName, inputValue) {
-    this.setState({[inputName]: inputValue})
-  }
-
-  async handleSubmit(event) {
-    event.preventDefault()
-    const {editedText, testCases} = this.state
-    const code = editedText
-    // will invoke the function once even if the user doesn't input anything
-    const inputs = testCases ? testCases.trim().split('\n') : ['undefined']
-    this.props.submitEditedText(editedText)
-    this.setState({
-      testCasesRunning: true
-    })
-    NProgress.start()
-    let inc = 0.1,
-      progress = 0.0
-    let incLoader = setInterval(function() {
-      progress += inc
-      NProgress.set(progress)
-    }, 1000)
-    try {
-      this.setState({
-        outputs: await this.evaluator.getResult(code, inputs),
-        testCasesRunning: false
-      })
-    } catch (error) {
-      this.setState({
-        outputs: `${error.name ? error.name + ': ' : ''}${error.message}`,
-        testCasesRunning: false
-      })
-    }
-    clearInterval(incLoader)
-    NProgress.done()
   }
 
   readFile() {
     if (!this.props.image) return
     try {
-      const {text} = this.props
       const fileReader = new FileReader()
       fileReader.onloadend = () => {
-        this.setState({image: fileReader.result, editedText: jBeautify(text)})
+        this.setState({image: fileReader.result})
       }
       fileReader.readAsDataURL(this.props.image)
       findOrientation(this.props.image, (err, orientation) => {
@@ -172,7 +124,9 @@ class EditPage extends Component {
   }
 
   render() {
-    const {editedText, testCases, outputs, image, imageClass} = this.state
+    const {image, imageClass} = this.state
+    const {editedText, testCases, testCasesRunning, outputs} = this.props
+    const {handleChange, handleSubmit} = this.props
     const {classes, error, loading} = this.props
     if (loading) return <Loading />
     else if (error) return <ErrorPage classes={classes} error={error} />
@@ -208,7 +162,7 @@ class EditPage extends Component {
                 >
                   <CodeMirror
                     editedText={editedText}
-                    handleChange={this.handleChange}
+                    handleChange={handleChange}
                   />
                 </Grid>
 
@@ -221,16 +175,13 @@ class EditPage extends Component {
                   xl={12}
                   className={classes.gridItems}
                 >
-                  <form
-                    onSubmit={this.handleSubmit}
-                    className={classes.littleGrid}
-                  >
+                  <form onSubmit={handleSubmit} className={classes.littleGrid}>
                     <InputOutputWrapper
-                      running={this.state.testCasesRunning}
+                      running={testCasesRunning}
                       testCases={testCases}
                       outputs={outputs}
-                      onChange={this.handleChange}
-                      handleSubmit={this.handleSubmit}
+                      onChange={handleChange}
+                      handleSubmit={handleSubmit}
                     />
                   </form>
                   <p />
@@ -248,19 +199,10 @@ class EditPage extends Component {
 }
 
 const mapStateToProps = state => {
-  const {text, editedText, image, error} = state.code
+  const {image, error} = state.code
   return {
-    text,
-    editedText,
     image,
-    error,
-    loading: state.loading
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    submitEditedText: editedText => dispatch(submitEditedText(editedText))
+    error
   }
 }
 
@@ -268,7 +210,4 @@ EditPage.propTypes = {
   classes: PropTypes.object.isRequired
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(EditPage))
+export default connect(mapStateToProps)(withStyles(styles)(editCode(EditPage)))
