51c6608c4b6c53f54fdd0d4e4b6001c503d2bb33
Michelle <mcurena24@gmail.com>
2019-02-08 14:07:33 -0500

    chore: add seed data and use username to greet on user-home

--- script/seed.js
+++ script/seed.js
@@ -8,8 +8,16 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
+    User.create({
+      name: 'Sophia',
+      email: 'sophia25@gmail.com',
+      password: '123'
+    }),
+    User.create({
+      name: 'Murphy',
+      email: 'murphy123@gmail.com',
+      password: '123'
+    })
   ])
 
   console.log(`seeded ${users.length} users`)
